8f24abed278695f122c2c033a7813258906d962f
Cody <lawdermiltcody@gmail.com>
2020-04-12 20:05:30 -0500

    Migration

--- db/migrate/20200413004124_create_chatrooms.rb
+++ db/migrate/20200413004124_create_chatrooms.rb
@@ -1,7 +1,8 @@
 class CreateChatrooms < ActiveRecord::Migration[6.0]
   def change
     create_table :chatrooms do |t|
-
+      t.text :body
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/migrate/20200413004130_create_sessions.rb
+++ db/migrate/20200413004130_create_sessions.rb
@@ -1,7 +1,7 @@
 class CreateSessions < ActiveRecord::Migration[6.0]
   def change
     create_table :sessions do |t|
-
+      t.string :username
       t.timestamps
     end
   end
