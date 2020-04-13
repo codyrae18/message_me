5e2f249673c7cec72b2ce52299b220bc04b89910
Cody <lawdermiltcody@gmail.com>
2020-04-13 18:28:39 -0500

    added flash style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,20 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully loged in"
+            redirect_to root_path
+        else
+            flash.now[:error] = "Incorrect username or password"
+            render 'new'
+        end
+    end
 
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have logged out"
+        redirect_to root_path
     end
 end
